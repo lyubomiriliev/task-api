1937324c390da4d98013f30bd156986da44a233d
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2025-02-04 14:32:49 +0200

    Add some styling and route links

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,9 +1,14 @@
+import "../../styles/task.scss";
+
 const Task = ({ task }) => {
   return (
-    <div>
-      <h3>{task.name}</h3>
-      <p>{task.priority}</p>
-      <p>{task.progress}</p>
+    <div className="task-container">
+      <h3 className="task-name">Title: {task.name}</h3>
+      <div>
+        <p className="task-priority">Priority: {task.priority}</p>
+        <p className="task-progress">Progress: {task.progress}</p>
+      </div>
+      <button className="finish-btn">Finish task</button>
     </div>
   );
 };
