afcc5de193c7a7d9c3bd6afeece1a7c269f058e4
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2025-02-08 00:34:29 +0200

    Add user model for authentication

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    emai: {
+      type: String,
+      required: [true, "Please add an email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", UserSchema);
